d820c331aa122f4ee3c06e780deafe1153c943c2
mena-mora <ximenaamadormorante@gmail.com>
2026-02-17 16:40:43 -0600

    Boton para regresar a Categorias

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -15,6 +15,9 @@
             <button type="submit" class="bg-blue-700 hover:bg-blue-800 text-white px-4 py-2 rounded-lg font-medium transition-all glow">
                 <i class="fas fa-save mr-2"></i> Guardar Categoría
             </button>
+            <button type="button" onclick="window.location.href='{{ route('categorias.index') }}'" class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg font-medium transition-all glow ml-2">
+                <i class="fas fa-arrow-left mr-2"></i> Volver a Categorías
+            </button>
         </form>
     </div>
 
